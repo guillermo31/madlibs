826a2d361d8a16639d146036e3d00ef94bc6e7f2
Guillermo Ramos <guillermoramos@Guillermos-MacBook-Pro.local>
2018-07-12 10:07:55 -0600

    functional components

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
   render() {
     return (
       <div className = "home">
-        {Header('madlibs', 'random paragraph my name is guillermo ramos ')}
+        {Header()}
 
       </div>
     );
